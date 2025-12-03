681c5873b8be89046a2ea86813f338f735f9bb59
Xurreus <78816572+Xurreus@users.noreply.github.com>
2025-12-03 16:03:09 +0200

    Create Utils.java

---
+++ src/test/java/com/qa/automation/Utils.java
@@ -0,0 +1,7 @@
+package com.qa.automation;
+
+public class Utils {
+    public static void printTestStatus(String testName, String status) {
+        System.out.println("[" + status + "] " + testName);
+    }
+}
